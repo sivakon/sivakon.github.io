74caff7dd76659cf115edc545abe81ae82ab8c75
Sivaram Konanki <ksiva+github@hey.com>
2020-11-17 21:05:53 +0900

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,3 @@
 <h1>About me</h1>
 
-<p>Experiments and everything I find online. You can find me on <a href="https://twitter.com/sivakon">twitter</a>, <a href="https://github.com/sivakon">github</a> and <a href="https://www.linkedin.com/in/sivaram-konanki">Linkedin</a></p>
+<p>Experiments and everything I find online. You can find me on <a href="https://twitter.com/iam_trickster">twitter</a>, <a href="https://github.com/trickster">github</a> and <a href="https://www.linkedin.com/in/sivaram-konanki">Linkedin</a></p>
